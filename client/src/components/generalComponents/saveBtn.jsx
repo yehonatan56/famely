367ca31ba161d7f55f54abf4377fe5de9d5aad43
Yehonatan <yehonatanrefael@gmail.com>
2024-07-12 16:52:06 +0300

    chore: Add saved notification to SaveBtn component

--- client/src/components/generalComponents/saveBtn.jsx
+++ client/src/components/generalComponents/saveBtn.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import { Button } from '@mantine/core'
+import React, { useState } from 'react';
+import { Button, Notification } from '@mantine/core';
 import { useSelector } from 'react-redux';
 import { getUserDataSelector } from '../../store/selectors/user.selector';
 import { saveUser } from '../../requests/user.proxy';
 export default function SaveBtn() {
   const user = useSelector(getUserDataSelector);
+  const [saved, setSaved] = useState(false);
+
   const save = () => {
+    setSaved(true);
     saveUser(user)
   }
   return (
     <div>
-        <Button onClick={() => save()} color="blue">Save</Button>
+      {saved && (
+        <Notification style={
+          { position: "absolute", right: 0, bottom: 0, margin: "10px" }
+        } title="saved sucsessful">
+          you can exit  from this page
+        </Notification>
+      )}
+      <Button onClick={() => save()} color="blue">Save</Button>
     </div>
   )
 }
