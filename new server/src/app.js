c6c1f9ca609ceab134be4198679e46c8ab90ef2f
Yehonatan <yehonatanrefael@gmail.com>
2024-08-06 17:02:52 +0300

    refactor: Update image handling in family members list

---
+++ new server/src/app.js
@@ -0,0 +1,10 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.listen(3002, () => console.log("server  run "));
